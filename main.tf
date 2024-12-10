f4ea90d6fb9b5c64ec4c6cedb34f223c0a79507f
Subrahmanya Gonella <satishgonella2024@gmail.com>
2024-12-10 20:35:03 +0000

    Initial commit with Terraform example

---
+++ main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+
+  backend "azurerm" {
+    resource_group_name  = "terraform-state-rg"
+    storage_account_name = "terraformstate123"
+    container_name       = "state"
+    key                  = "terraform.tfstate"
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "East US"
+}
